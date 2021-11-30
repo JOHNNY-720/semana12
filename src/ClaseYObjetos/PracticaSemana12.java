9fb89897358d6ad55213e63b436ff0441ec8e08f
JOHNNY-720 <johnnysandoval10@gmail.com>
2021-11-30 16:51:44 -0600

    actualizar

--- src/ClaseYObjetos/PracticaSemana12.java
+++ src/ClaseYObjetos/PracticaSemana12.java
@@ -1,31 +1,15 @@
-
 package ClaseYObjetos;
 
 import java.util.Scanner;
-public class PracticaSemana12 {
-        
-        public static void main (String [] dada){
-            marcadores Marcadores = new marcadores();
-            Scanner entrada= new Scanner(System.in);
-            
-            System.out.println("Cual seria la punta:");
-         Marcadores.punta=entrada.next();
-            
-            System.out.println(" Cual seria la marca:");
-             Marcadores.Marca=entrada.next();
-             System.out.println("Cual seria el color:");
-             Marcadores.color=entrada.next();
-             System.out.println("Cual seria la forma:");
-             Marcadores.forma=entrada.next();
-             System.out.println("Cual seria el material:");
-             Marcadores.material =entrada.next();
-             System.out.println("Cual seria el tamaño:");
-             Marcadores.tamaño=entrada.next();
-             System.out.println("Cual seria el precio:");
-             Marcadores.precio =entrada.nextInt();
-             
-             Marcadores. Impresion(); 
-        }
-                
-}
 
+public class PracticaSemana12 {
+
+    public static void main(String[] dada) {
+        marcadores Marcadores = new marcadores();
+
+        Marcadores.inicializar();
+
+        Marcadores.Impresion();
+    }
+
+}
--- src/ClaseYObjetos/marcadores.java
+++ src/ClaseYObjetos/marcadores.java
@@ -1,25 +1,100 @@
-
 package ClaseYObjetos;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class marcadores {
-  String punta;
-   String Marca;
-   String color;
-   String forma;
-   String material;
-   String tamaño;
-  int precio;
-    
-   
-    public void Impresion(){
-        System.out.println("Tipo de punta es: "+punta);
-        System.out.println("Marca es: "+ Marca);
-        System.out.println("Color es :  "+ color);
-        System.out.println("Forma es: "+ forma);
-        System.out.println("Tipo de material es: "+ material);
-        System.out.println("Tamaño es: "+ tamaño);
-        System.out.println("Precio es :"+ precio);
+
+    private Scanner entrada;
+    private String punta;
+    private String Marca;
+    private String color;
+    private String forma;
+    private String material;
+    private String tamaño;
+    private int precio;
+
+    public void inicializar() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Cual seria la punta:");
+        punta = entrada.next();
+
+        System.out.println(" Cual seria la marca:");
+        Marca = entrada.next();
+        System.out.println("Cual seria el color:");
+        color = entrada.next();
+        System.out.println("Cual seria la forma:");
+        forma = entrada.next();
+        System.out.println("Cual seria el material:");
+        material = entrada.next();
+        System.out.println("Cual seria el tamaño:");
+        tamaño = entrada.next();
+        System.out.println("Cual seria el precio:");
+        precio = entrada.nextInt();
+    }
+
+    public void Impresion() {
+        System.out.println("Tipo de punta es: " + punta);
+        System.out.println("Marca es: " + Marca);
+        System.out.println("Color es :  " + color);
+        System.out.println("Forma es: " + forma);
+        System.out.println("Tipo de material es: " + material);
+        System.out.println("Tamaño es: " + tamaño);
+        System.out.println("Precio es :" + precio);
+    }
+
+    public String getPunta() {
+        return punta;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPunta(String punta) {
+        this.punta = punta;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
     }
 }
