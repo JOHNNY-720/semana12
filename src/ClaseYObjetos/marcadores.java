bfd045b2469d5f26f8db6e2aa0d4aaa6ca8c6e02
JOHNNY-720 <johnnysandoval10@gmail.com>
2021-11-30 16:08:49 -0600

    Prueba1

---
+++ src/ClaseYObjetos/marcadores.java
@@ -0,0 +1,25 @@
+
+package ClaseYObjetos;
+
+import javax.swing.JOptionPane;
+
+public class marcadores {
+  String punta;
+   String Marca;
+   String color;
+   String forma;
+   String material;
+   String tamaño;
+  int precio;
+    
+   
+    public void Impresion(){
+        System.out.println("Tipo de punta es: "+punta);
+        System.out.println("Marca es: "+ Marca);
+        System.out.println("Color es :  "+ color);
+        System.out.println("Forma es: "+ forma);
+        System.out.println("Tipo de material es: "+ material);
+        System.out.println("Tamaño es: "+ tamaño);
+        System.out.println("Precio es :"+ precio);
+    }
+}
